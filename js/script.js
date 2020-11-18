99b53a21e09a9eba383642c5b94c2252bbac85b6
Mirko Guerra <mirkoguerra93@gmail.com>
2020-11-18 17:27:23 +0100

    Aggiunta

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
+// collego il codice di Vue all'html
 const toDoListApp = new Vue ({
   el: "#to-do-list",
   data: {
-
+    // inizializzo l'array che conterrà i list item che vengono generati al click del button
+    toDoListArray: [],
+    textInput: ""
   },
-  method: {
-    
+  methods: {
+    // funzione che aggiunge, al click del button, il list item generato al toDoListArray
+    addToToDoListArray: function(){
+      if (this.textInput != ""){
+        this.toDoListArray.push(this.textInput);
+      }
+		}
   }
 });
